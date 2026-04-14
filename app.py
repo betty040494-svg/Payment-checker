65463e97193a5c45df3bc8268034f39c50053305
betty040494-svg <betty040494@gmail.com>
2026-04-14 17:19:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -156,8 +156,9 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=help_msg, quick_reply=menu))
         return
 
-    # 預設歡迎訊息
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="👋 哈囉 Liao！我是您的校園金流管家，聚餐分帳、個人記帳都能幫您搞定！", quick_reply=menu))
+     # 🌟 預設歡迎訊息：現在會說「哈囉 [名字]！」
+    welcome_text = f"👋 哈囉 {user_name}！我是您的明細管家。\n\n點選下方按鈕，或輸入「幫助」來查看指令教學吧！"
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=welcome_text, quick_reply=menu))
 
 if __name__ == "__main__":
     app.run()
